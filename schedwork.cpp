86619bccccc5fa59fd3a1ac7759cf0a42163a66c
Edward Shao <shaoe@usc.edu>
2024-04-16 10:13:01 -0700

    finished wordle and sschedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,8 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool helper(const AvailabilityMatrix& avail, long unsigned int currRow, long unsigned int currCol, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, std::map<Worker_T, int> maxStatus);
 
+bool checkMaxShiftStatus(std::map<Worker_T, int>& maxStatus, Worker_T id, const size_t maxShifts); 
 
+void insertWorker(DailySchedule& sched, long unsigned int currRow, std::map<Worker_T, int>& maxStatus, Worker_T workerID); 
+
+void removeWorker(DailySchedule& sched, long unsigned int currRow, std::map<Worker_T, int>& maxStatus, Worker_T workerID);
+
+bool checkIfSameDay(DailySchedule& sched, long unsigned int currRow, long unsigned int currCol, Worker_T workerID); 
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -38,8 +45,115 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // A map that stores pairs of all workers and their # of filled shifts
+    std::map<Worker_T, int> maxStatus; 
 
+    // First (1) for loop - Loop through the avilability columns and insert each worker by their col # as their ID and 0 as starting value of filled shifts
+    for (long unsigned int i = 0; i < avail.at(0).size(); ++i){
+        maxStatus.insert(std::pair<Worker_T, int>(static_cast<Worker_T>(i), 0)); 
+    }
 
+    // Second (2) for loop - Create row vectors for all days 
+    for (long unsigned int i = 0; i < avail.size(); ++i){
+        sched.push_back(std::vector<Worker_T>());
+    }
 
+    // Start with row 0, col 0
+    return helper(avail, 0, 0, dailyNeed, maxShifts, sched, maxStatus); 
+    
+}
+
+bool helper(const AvailabilityMatrix& avail, long unsigned int currRow, long unsigned int currCol, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, std::map<Worker_T, int> maxStatus){
+    
+    // Base case, If currRow is greater than the # of days, that means we reached the end
+    if (currRow >= avail.size()){
+        return true; 
+    }
+
+    // If currCol is greater than the dailyNeed then that means we filled the day, move on to next row/day
+    // This also ensures that we don't push more than dailyNeed in a single row
+    if (currCol >= dailyNeed){
+        return helper(avail, currRow + 1, 0, dailyNeed, maxShifts, sched, maxStatus); 
+    }
+ 
+    // Third (3) for loop - Loop through each worker for the day - This should be the same for all days
+    for (long unsigned int i = 0; i < avail.at(currRow).size(); ++i){
+        
+        // If the worker is available and the worker hasn't reached their maxShifts yet and the worker is not already booked for that day
+        // (A worker cannot work twice the same day since they're only available once that day)
+        // (If worker is not available or the worker reached their maxShifts, go to next worker) 
+        if (avail.at(currRow).at(i) == 1 && checkMaxShiftStatus(maxStatus, i, maxShifts) == false && checkIfSameDay(sched, currRow, 0, i) == true){
+            // Insert worker
+            insertWorker(sched, currRow, maxStatus, i); 
+
+            // If the next recursive call returns true, then return true
+            if (helper(avail, currRow, currCol+1, dailyNeed, maxShifts, sched, maxStatus) == true){
+                return true; 
+            }
+
+            // If the next recursive call returns false, then remove the worker and continue the for loop for the next worker
+            else{
+                removeWorker(sched, currRow, maxStatus, i);
+                continue;
+            }
+        }
+    }
+
+    // if the for loop concludes, that means that no worker was found so return false
+    return false;
+}
+
+// Checks if a worker has reached its max shift already
+// Returns true is so
+// Returns false if not
+bool checkMaxShiftStatus(std::map<Worker_T, int>& maxStatus, Worker_T workerID, const size_t maxShifts){
+    // Uses iterator to find the user in the map
+    std::map<Worker_T, int>::iterator it = maxStatus.find(workerID); 
+    
+    // if (it == maxStatus.end()){
+    //     return true; 
+    // }
+
+    if (it->second >= static_cast<int>(maxShifts)){
+        return true; 
+    }
+
+    return false;
+}
+
+// Inserts worker into the sched vector and taking care of updating the person's shift status
+void insertWorker(DailySchedule& sched, long unsigned int currRow, std::map<Worker_T, int>& maxStatus, Worker_T workerID){
+    
+    // Insert a worker into sched
+    sched.at(currRow).push_back(workerID); 
+
+    // Increase worker maxStatus
+    std::map<Worker_T, int>::iterator it = maxStatus.find(workerID); 
+    ++(it->second);
+}
+
+// Removes  worker into the sched vector and taking care of updating the person's shift status
+void removeWorker(DailySchedule& sched, long unsigned int currRow, std::map<Worker_T, int>& maxStatus, Worker_T workerID){
+    
+
+    // Remove the last worker in row
+    sched.at(currRow).pop_back(); 
+
+    // Decrease worker maxStatus
+    std::map<Worker_T, int>::iterator it = maxStatus.find(workerID); 
+    --(it->second);
+}
+
+// A worker cannot work twice in the same day, so check if a worker is already working/scheduled in the day/row already
+bool checkIfSameDay(DailySchedule& sched, long unsigned int currRow, long unsigned int currCol, Worker_T workerID){
+    if (currCol >= sched.at(currRow).size()){
+        return true; 
+    }
+
+    if (sched.at(currRow).at(currCol) == workerID){
+        return false; 
+    }
+
+    return checkIfSameDay(sched, currRow, currCol + 1, workerID); 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void findWord(std::set<std::string>::iterator it, std::set<std::string>& result, const std::string& in, const std::string& floating, const std::set<std::string>& dict); 
+bool checkIfUpper(const std::string& input); 
+bool checkAllFloating(const std::string& dict, const std::string& floating);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,120 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    
+    // Final result
+    std::set<std::string> result; 
 
+    // Iterator to point to the first word in the dictionary
+    std::set<std::string>::iterator dictionaryIterator = dict.begin();
+
+    // Call recursive helper
+    findWord(dictionaryIterator, result, in, floating, dict); 
+
+    return result;
 }
 
 // Define any helper functions here
+void findWord(std::set<std::string>::iterator dictionaryIterator, std::set<std::string>& result, const std::string& in, const std::string& floating, const std::set<std::string>& dict){
+    // Base case, if the dictionary iterator is at the end, that means we have went through all the list
+    // Therefore return true
+    if (dictionaryIterator == dict.end()){
+        return; 
+    }
+
+    // If the current word that dictionaryIterator does not match the length of the input word
+    // Skip word by recursing and ++
+    if (dictionaryIterator->length() != in.length()){
+        findWord(++dictionaryIterator, result, in, floating, dict); 
+    }
+
+    // If the current word matches the length
+    else if (dictionaryIterator->length() == in.length()){
+        // A boolean to check if the all floating letters are present in curr word
+        // bool floatingStatus = false; 
+        
+
+        // First (1) for loop
+        // Loop through the curr dictionary word (or input word since they're same length)
+        // If the current location is not empty, '-', check the same letter in dict is in the same position 
+        // as input
+        for (long unsigned int i = 0; i < dictionaryIterator->length(); ++i){
+
+            // If not, move on to next word in dict 
+            if (in.at(i) != '-'){
+                if (in.at(i) != dictionaryIterator->at(i)){
+                    findWord(++dictionaryIterator, result, in, floating, dict); 
+                    return;
+                }
+            }
+        }
+
+        // Returns true if all words in floating is present in the curr dictionary word in separate funciton
+        bool checkFloat = checkAllFloating(*dictionaryIterator, floating); 
+        
+        // If not, move on to next word in dict 
+        if (checkFloat == false){
+            findWord(++dictionaryIterator, result, in, floating, dict); 
+            return;
+        }
+
+        // If true
+        else if (checkFloat == true){
+
+            // Use separate function to see if there are any uppercase,
+            // If not uppercase, insert it to result set
+            if (checkIfUpper(*dictionaryIterator) == false){
+                result.insert(*dictionaryIterator);
+            }
+            
+            // Move on to next word in dict
+            findWord(++dictionaryIterator, result, in, floating, dict); 
+            return; 
+        }
+    }
+}
+
+bool checkIfUpper(const std::string& input){
+    // Second (2) for loop
+
+    // Loop through the word to see if there are any uppercase, return true if so
+    for (long unsigned int i = 0; i < input.size(); ++i){
+        if (input.at(i) >= 'A' && input.at(i) <= 'Z'){
+            return true;
+        }
+    }
+
+    return false; 
+}
+
+bool checkAllFloating(const std::string& dict, const std::string& floating){
+    // Base case, If floating string is empty, return true
+    if (floating.size() == 0){
+        return true;
+    }
+
+    // Base case, if the dictionary size is 0, that means that floating > dict, so this word does not contain all letters of floating
+    if (dict.size() == 0){
+        return false; 
+    }
+
+    // Third (3) for loop 
+    // Loop through the current dictionary word
+    for (long unsigned int i = 0; i < dict.size(); ++i){
+
+        // If the current floating letter is equal to the dictionary letter, 
+        // Create a new string that removes that letter using substr
+        if (floating.at(0) == dict.at(i)){
+            std::string newDict = dict.substr(0, i);
+
+            if (i != dict.size() -1){
+                newDict += dict.substr(i+1, dict.size()-1); 
+            } 
+
+            // recurse with new substr of dict as well as a substring of floating that removes the front letter
+            return checkAllFloating(newDict, floating.substr(1, floating.size()-1));
+        }
+    }
+
+    return false; 
+}
